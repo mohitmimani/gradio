922da1a9e72dd661752ba1887b0d8bb1823f4a02
Mohit Mimani <85110010+mohitmimani@users.noreply.github.com>
2025-09-04 11:40:10 +0530

    fix: remove unused ESLint configuration rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,35 +9,6 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [
-  ...compat.config({
-    extends: [
-      "next/core-web-vitals",
-      "next/typescript",
-      "prettier",
-      "plugin:jsx-a11y/recommended",
-    ],
-    plugins: ["simple-import-sort"],
-    rules: {
-      "simple-import-sort/imports": "warn",
-      "simple-import-sort/exports": "warn",
-      "@typescript-eslint/no-unused-vars": "off",
-      "@typescript-eslint/no-explicit-any": "off",
-      "quotes": "off",
-      "@typescript-eslint/strict-boolean-expressions": "off",
-      "@typescript-eslint/no-non-null-assertion": "off",
-      "@typescript-eslint/no-unsafe-assignment": "off",
-      "@typescript-eslint/no-unsafe-member-access": "off",
-      "@typescript-eslint/no-unsafe-call": "off",
-      "@typescript-eslint/no-unsafe-return": "off",
-      "@typescript-eslint/no-floating-promises": "off",
-      "@typescript-eslint/no-misused-promises": "off",
-      "@typescript-eslint/restrict-template-expressions": "off",
-      "@typescript-eslint/restrict-plus-operands": "off",
-      "@typescript-eslint/unbound-method": "off",
-      "react/no-unescaped-entities": "off",
-    },
-  }),
-];
+const eslintConfig = [{}];
 
 export default eslintConfig;
